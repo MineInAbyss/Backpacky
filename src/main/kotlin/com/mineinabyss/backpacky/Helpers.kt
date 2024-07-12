eeacbdd1b99dbb3e4f766ecda68f481e398fa332
Boy <sivertpaulsen2@gmail.com>
2024-07-13 01:01:39 +0200

    feat: allow for recipes to work even when backpacks have items in them

--- src/main/kotlin/com/mineinabyss/backpacky/Helpers.kt
+++ src/main/kotlin/com/mineinabyss/backpacky/Helpers.kt
@@ -1,10 +1,13 @@
 package com.mineinabyss.backpacky
 
+import com.mineinabyss.backpacky.components.Backpack
 import com.mineinabyss.geary.datatypes.Component
 import com.mineinabyss.geary.datatypes.GearyEntity
+import com.mineinabyss.geary.papermc.datastore.has
 import com.mineinabyss.geary.papermc.tracking.items.gearyItems
 import com.mineinabyss.idofront.nms.nbt.fastPDC
 import org.bukkit.inventory.ItemStack
 
 internal fun ItemStack.toGearyOrNull(): GearyEntity? = gearyItems.itemProvider.deserializeItemStackToEntity(this.fastPDC)
 internal inline fun <reified T : Component> ItemStack.decode(): T? = gearyItems.itemProvider.deserializeItemStackToEntity(this.fastPDC)?.get<T>()
+fun ItemStack.isBackpack() = gearyItems.itemProvider.deserializeItemStackToEntity(this.fastPDC)?.has<Backpack>() == true
--- src/main/kotlin/com/mineinabyss/backpacky/listeners/BackpackListener.kt
+++ src/main/kotlin/com/mineinabyss/backpacky/listeners/BackpackListener.kt
@@ -5,36 +5,68 @@ import com.github.shynixn.mccoroutine.bukkit.ticks
 import com.mineinabyss.backpacky.backpacky
 import com.mineinabyss.backpacky.components.Backpack
 import com.mineinabyss.backpacky.components.BackpackContent
+import com.mineinabyss.backpacky.isBackpack
 import com.mineinabyss.backpacky.toGearyOrNull
-import com.mineinabyss.geary.papermc.datastore.encodeComponentsTo
-import com.mineinabyss.geary.papermc.datastore.has
+import com.mineinabyss.geary.papermc.datastore.*
+import com.mineinabyss.geary.papermc.features.items.recipes.DenyInVanillaRecipes
+import com.mineinabyss.geary.papermc.tracking.items.gearyItems
 import com.mineinabyss.geary.papermc.tracking.items.inventory.toGeary
+import com.mineinabyss.geary.prefabs.PrefabKey
 import com.mineinabyss.geary.serialization.getOrSetPersisting
 import com.mineinabyss.geary.serialization.setPersisting
+import com.mineinabyss.idofront.items.editItemMeta
+import com.mineinabyss.idofront.messaging.broadcast
+import com.mineinabyss.idofront.messaging.broadcastVal
+import com.mineinabyss.idofront.nms.nbt.fastPDC
+import com.mineinabyss.idofront.serialization.toSerializable
 import dev.triumphteam.gui.components.GuiType
 import dev.triumphteam.gui.guis.Gui
 import kotlinx.coroutines.delay
+import org.bukkit.Bukkit
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.ClickType
+import org.bukkit.event.inventory.PrepareItemCraftEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerSwapHandItemsEvent
 import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
+import org.bukkit.inventory.ShapedRecipe
 
 class BackpackListener : Listener {
 
     @EventHandler
+    fun PrepareItemCraftEvent.onUpgradeBackpack() {
+        if (recipe != null || inventory.matrix.none { it?.isBackpack() == true }) return
+
+        val matrix = inventory.matrix.map { it?.clone()?.editItemMeta { persistentDataContainer.remove<BackpackContent>() } }.toTypedArray()
+        val recipesWithBackpack = Bukkit.recipeIterator().asSequence()
+            .filterIsInstance<ShapedRecipe>().filter { it.result.isBackpack() }
+        val customRecipeResult = recipesWithBackpack.filter { recipe ->
+            val ingredients = recipe.shape.flatMap { it.mapNotNull { c -> recipe.choiceMap[c] } }
+            ingredients.zip(matrix).all { it.first.test(it.second ?: ItemStack.empty()) }
+        }.firstOrNull()
+
+        val backpack = inventory.matrix.first { it?.isBackpack() == true }?.itemMeta?.persistentDataContainer?.decode<BackpackContent>() ?: return
+        inventory.result = customRecipeResult?.result?.editItemMeta {
+            persistentDataContainer.encode(backpack)
+        }
+
+    }
+
+    @EventHandler
     fun PlayerInteractEvent.onOpenBackpack() {
         // Disable from placing blocks etc with offhand if a backpack is being opened
-        if (hand == EquipmentSlot.OFF_HAND && player.inventory.itemInMainHand.toGearyOrNull()?.has<Backpack>() == true)
+        if (hand == EquipmentSlot.OFF_HAND && player.inventory.itemInMainHand.isBackpack())
             setUseItemInHand(Event.Result.DENY)
 
         val (item, gearyItem) = (item ?: return) to (item?.toGearyOrNull() ?: return)
         val backpack = gearyItem.get<Backpack>() ?: return
         val backpackContent = gearyItem.getOrSetPersisting { BackpackContent() }
-        val title = backpack.title ?: item.itemMeta?.displayName().takeIf { item.itemMeta.hasDisplayName() } ?: item.itemMeta.itemName()
+        val title = backpack.title ?: item.itemMeta?.displayName().takeIf { item.itemMeta.hasDisplayName() }
+        ?: item.itemMeta.itemName()
 
         val backpackGui = Gui.gui(backpack.guiType).title(title).let {
             if (backpack.guiType == GuiType.CHEST) it.rows(backpack.rows.coerceIn(1, 6)) else it
@@ -53,10 +85,10 @@ class BackpackListener : Listener {
         }
 
         backpackGui.setDefaultClickAction {
-            if (it.click == ClickType.SWAP_OFFHAND && player.inventory.itemInOffHand.toGearyOrNull()?.has<Backpack>() == true) it.isCancelled = true
-            if (it.clickedInventory is PlayerInventory && player.inventory.toGeary()?.get(it.slot)?.has<Backpack>() == true) it.isCancelled = true
-            if (it.cursor.itemMeta?.persistentDataContainer?.has<Backpack>() == true) it.isCancelled = true
-            if (it.currentItem?.itemMeta?.persistentDataContainer?.has<Backpack>() == true) it.isCancelled = true
+            if (it.click == ClickType.SWAP_OFFHAND && player.inventory.itemInOffHand.isBackpack()) it.isCancelled = true
+            if (it.clickedInventory is PlayerInventory && player.inventory.toGeary()?.get(it.slot)?.has<Backpack>() == true)
+                it.isCancelled = true
+            if (it.cursor.isBackpack() || it.currentItem?.isBackpack() == true) it.isCancelled = true
 
             backpacky.launch {
                 delay(1)
